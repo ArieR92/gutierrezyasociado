30ebbefc0dcfec7e7544a04e809f61e6988a0a38
ArielR92 <arielrodriguez0@outlook.com>
2022-10-23 18:01:53 -0600

    commint-final-1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //secondary carousel//
 
 $(".owl-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     autoplayhoverpause: true,
     autoplaytimeout: 100,
     items: 3,
@@ -27,6 +27,28 @@ $(".owl-carousel").owlCarousel({
     }
 
 });
+
+//FORM VALIDATION//
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
 
 
 
