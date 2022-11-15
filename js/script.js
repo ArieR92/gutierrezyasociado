74c34b0b4a607f16db15a98dba035d9a542a9f1a
ArielR92 <arielrodriguez0@outlook.com>
2022-11-14 23:02:21 -0600

    commit-button-leerMas

--- js/script.js
+++ js/script.js
@@ -53,3 +53,12 @@ $(".owl-carousel").owlCarousel({
 
 
 
+
+
+$('.readmore-btn').on('click', function(){
+  $(this).parent().toggleClass('ShowContent');
+
+  var replaceText = $(this).parent().hasClass('ShowContent') ? "Leer menos" : "Leer más";
+  $(this).text(replaceText);
+
+});
